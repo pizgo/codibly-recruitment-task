17d42ea4e25c8ced399d93b00210cd4bb4e61ce1
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-01-18 16:29:58 +0100

    extracted shared logic for handling user data into a new custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import PaginateButtons from "./components/PaginateButtons";
 import SingleProduct from "./components/SingleProduct";
 import { Product } from "./types/interfaces";
 import { searchParams } from './consts/urlParams';
-import { useNavigateSearch } from "./hooks/UseNavigateSearch";
 import { useFetchProducts } from "./hooks/useFetchProducts";
+import {useUserInteracted} from "./hooks/useUserInteracted";
 
 const App: React.FC = () => {
     const initialProductId = searchParams.get("id") || "";
@@ -15,30 +15,21 @@ const App: React.FC = () => {
 
     const { productsState, errorMessage, callForData } = useFetchProducts(
         initialProductId,
-        initialPageNumber,
-    );
+        initialPageNumber,);
 
-    const [filteredId, setFilteredId] = useState<string>(initialProductId)
-    const [pageNumber, setPageNumber] = useState<number>(initialPageNumber)
+    const { handleInteraction, filteredId, pageNumber } = useUserInteracted(initialPageNumber, initialProductId, callForData)
+
     const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
     const [chosenProduct, setChosenProduct]= useState<Product>();
-    const navigateSearch = useNavigateSearch()
 
     const handleFilterIdChange = (enteredId : string) : void => {
-        handleInteration(1, enteredId)
+        handleInteraction(1, enteredId)
     }
 
     const handleArrowClick = (dir: "prev" | "next") => {
         const newPageNumber : number =
             dir === "next" ? pageNumber + 1 : pageNumber - 1;
-        handleInteration(newPageNumber, filteredId)
-    }
-
-    const handleInteration = (newPageNumber : number, filteredId : string ) => {
-        setPageNumber(newPageNumber);
-        setFilteredId(filteredId)
-        navigateSearch(filteredId, newPageNumber)
-        callForData(filteredId, newPageNumber)
+        handleInteraction(newPageNumber, filteredId)
     }
 
     const handleChooseProduct = (item: Product) : void => {
---
+++ src/hooks/useUserInteracted.tsx
@@ -0,0 +1,20 @@
+import {useState} from "react";
+import {useNavigateSearch} from "./useNavigateSearch";
+
+export const useUserInteracted = (
+    initialPageNumber: number,
+    initialProductId: string
+    , callForData: (id: string, page: number) => void) => {
+    const [pageNumber, setPageNumber] = useState<number>(initialPageNumber)
+    const [filteredId, setFilteredId] = useState<string>(initialProductId)
+    const navigateSearch = useNavigateSearch()
+
+    const handleInteraction = (newPageNumber : number, filteredId : string ) => {
+        setPageNumber(newPageNumber);
+        setFilteredId(filteredId)
+        navigateSearch(filteredId, newPageNumber)
+        callForData(filteredId, newPageNumber)
+    }
+
+    return {handleInteraction, filteredId, pageNumber}
+}
