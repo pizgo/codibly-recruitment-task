280c347dc839942919b33d2893b56e4e32651f56
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-01-18 23:48:09 +0100

    cleaned up the code

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,10 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom"
 
-
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>
-        ,
-
+    </React.StrictMode>,
     document.getElementById('root'));
